62e1f71a45f8301e7126a0e255b6f12adfdf5520
Tprezioso <tommyprezioso@gmail.com>
2017-02-02 14:06:39 -0500

    added welcomeVC and added hiddenButton to 2.0

---
+++ TicTacToe2.0/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  TicTacToe2.0
+//
+//  Created by Thomas Prezioso on 2/2/17.
+//  Copyright © 2017 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet var welcomeLabel: UILabel!
+
+    @IBAction func twoPlayerButton(_ sender: Any) {
+    }
+    @IBAction func hiddenTwoPointOButton(_ sender: Any) {
+    }
+}
